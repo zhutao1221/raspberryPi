e24b605dcb79852ac0d2891e7a8c8029161de701
zhutao <zhutao1221@126.com>
2018-06-19 11:02:32 +0800

    AipFace4IpCam

--- src/AipFace4IpCam.py
+++ src/AipFace4IpCam.py
@@ -5,9 +5,6 @@ import numpy as np
 from aip import AipFace
 import base64
 import multiprocessing
-
-
-# reTemp = str(base64.b64encode(bs)
       
 stream=urllib.urlopen('http://admin:admin@192.168.0.143:8081/video')
 bytes=''
@@ -21,7 +18,7 @@ groupIdList = "hz"
 client = AipFace(APP_ID, API_KEY, SECRET_KEY)
 
 def execClient(ijpg):
-    result = client.search(jpg2, imageType, groupIdList)
+    result = client.search(ijpg, imageType, groupIdList)
     print(result)
 
 index = 0
