e3e7042b05cef35c34d284c15debcfca9fd71efb
zhutao <zhutao1221@126.com>
2018-10-18 11:22:54 +0800

    alert

---
+++ imageAI/src/alert.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import winsound
+import MySQLdb
+import time
+
+while True:
+    db = MySQLdb.connect("127.0.0.1", "root", "root", "app",charset='utf8')
+    cursor = db.cursor()
+    cursor.execute("SELECT alert FROM notice")
+    results = cursor.fetchone()
+
+    if results is None:
+        print('ok')
+    elif results[0] == 1:
+        print('got you!')   
+        winsound.PlaySound('alert.wav',flags=1)
+        time.sleep(5)
+    db.close
+    time.sleep(2)
